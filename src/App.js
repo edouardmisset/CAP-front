24a2ad49105a0ad9b9dcd5419c5c8df137678399
Edouard Misset <edouardmisset@gmail.com>
2021-07-21 10:51:38 +0200

    Initialization

---
+++ src/App.js
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  )
+}
