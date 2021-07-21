b04f32647f5e46e9544e357320ec0ada327e8120
Edouard Misset <edouardmisset@gmail.com>
2021-07-21 11:38:07 +0200

    Simple home page with some indications for the user

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+export default function HomePage() {
+  return (
+    <>
+      <h2>Home</h2>
+      <p>Welcom to the jungle 🎶. This is the home page.</p>
+      <p>
+        Try to log a few ascents, then go the the dashboard page to see your
+        graphs
+      </p>
+    </>
+  )
+}
