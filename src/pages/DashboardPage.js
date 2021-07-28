edeafcb389522ad0ecdb85a9c25bdbab9ebd2596
Edouard Misset <edouardmisset@gmail.com>
2021-07-28 16:48:29 +0200

    Moved the table of ascents to another file

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
-import dayjs from 'dayjs'
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 import { useEffect, useState } from 'react'
 import API from '../APIClient'
+import AscentTable from '../components/AscentTable'
 import { graphOptions } from '../utilities/graphHelper'
 
 const { CancelToken } = axios
@@ -68,55 +68,7 @@ export default function DashboardPage() {
           options={ascentsByGradeByStyleOptions}
         />
       </div>
-      <div className="table">
-        <h3>Ascents</h3>
-        <div className="table-filter">
-          <p>Climber: {ascentList[0]?.climber}</p>
-          <p>Route / Boulder: {ascentList[0]?.routeOrBoulder}</p>
-        </div>
-        <p>
-          {`${ascentList.length} ascents over
-          ${new Set(ascentList.map((ascent) => ascent.date)).size} days`}
-        </p>
-        <table className="ascent-table">
-          <thead>
-            <tr>
-              <th>Route Name</th>
-              <th>Topo Grade</th>
-              <th>Number Of Tries</th>
-              <th>Crag</th>
-              <th>Date</th>
-              <th>Route / Boulder</th>
-              <th>Climber</th>
-            </tr>
-          </thead>
-          <tbody>
-            {!!ascentList.length &&
-              ascentList.map(
-                ({
-                  routeName,
-                  topoGrade,
-                  date,
-                  crag,
-                  climber,
-                  routeOrBoulder,
-                  numberOfTries,
-                  id,
-                }) => (
-                  <tr key={id}>
-                    <td>{routeName}</td>
-                    <td>{topoGrade}</td>
-                    <td>{numberOfTries}</td>
-                    <td>{crag}</td>
-                    <td>{dayjs(date).format('YYYY/MM/DD')}</td>
-                    <td>{routeOrBoulder}</td>
-                    <td>{climber}</td>
-                  </tr>
-                )
-              )}
-          </tbody>
-        </table>
-      </div>
+      <AscentTable ascentList={ascentList} />
     </>
   )
 }
