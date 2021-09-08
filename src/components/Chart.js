f58a2a16da748ff0c7fc38bbc9f366c2bbab62de
Edouard Misset <edouardmisset@gmail.com>
2021-09-08 19:00:31 +0200

    commented the setChartData fct

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -4,6 +4,14 @@ import { graphOptions } from '../utilities/graphHelper'
 
 // Create a new chart using Highcharts and the default options from the graphHelper.js and props
 export default function Chart({ x, y, title = '', stacking = null }) {
+  /**
+   * @description Returns a Highcharts options object with the given input and graphHelper's options
+   * @date 08/09/2021
+   * @param {Array} xChart - The x-axis data
+   * @param {Array} yChart - The y-axis data
+   * @param {String} chartTitle - The title of the chart
+   * @param {Boolean} chartStacking - The stacking of the chart
+   */
   const setChartData = (xChart, yChart, chartTitle, chartStacking) => ({
     ...graphOptions,
     chart: {
