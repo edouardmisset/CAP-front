2b07c0b2c82d04d8e2dc3bc03df82fe210920b7d
Edouard Misset <edouardmisset@gmail.com>
2021-09-08 18:44:07 +0200

    Cleanup and refacto

--- src/APIClient.js
+++ src/APIClient.js
@@ -1,26 +1,8 @@
 import axios from 'axios'
-// import browserHistory from './history'
 
 const API = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
   // withCredentials: true,
 })
-
-// API.interceptors.response.use(
-//   (res) => res,
-//   (err) => {
-//     if (
-//       err.response &&
-//       (err.response.status === 401 || err.response.status === 403) &&
-//       window.location.pathname !== '/' &&
-//       window.location.pathname !== '/password-reset-instructions' &&
-//       window.location.pathname !== '/reset-password'
-//     ) {
-//       browserHistory.push(`/?redirectUrl=${window.location.pathname}`)
-//       window.localStorage.setItem('logged', false)
-//     }
-//     return Promise.reject(err)
-//   }
-// )
 
 export default API
--- src/components/AscentTable.js
+++ src/components/AscentTable.js
@@ -1,48 +1,59 @@
 import dayjs from 'dayjs'
 
 export default function AscentTable({ ascentList }) {
-  return ascentList.length > 0 ? (
-    <div className="table">
-      <h3>Ascents</h3>
-      <p>
-        {`${ascentList.length} ascents over
-          ${new Set(ascentList.map((ascent) => ascent.date)).size} days`}
-      </p>
-      <table className="ascent-table">
-        <thead>
-          <tr>
-            <th>Route Name</th>
-            <th>Topo Grade</th>
-            <th>Number Of Tries</th>
-            <th>Crag</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {!!ascentList.length &&
-            ascentList.map(
-              ({
-                routeName,
-                topoGrade,
-                date,
-                crag,
-                routeOrBoulder,
-                numberOfTries,
-                id,
-              }) => (
-                <tr key={id}>
-                  <td>
-                    {`${routeOrBoulder === 'route' ? '🧗‍♂️' : '🪨'} ${routeName}`}
-                  </td>
-                  <td>{topoGrade}</td>
-                  <td>{numberOfTries}</td>
-                  <td>{crag}</td>
-                  <td>{dayjs(date).format('YYYY-MM-DD')}</td>
-                </tr>
-              )
-            )}
-        </tbody>
-      </table>
-    </div>
-  ) : null
+  const numberOfAscents = ascentList.length
+
+  const numberOfAscentDays = new Set(ascentList.map(({ date }) => date)).size
+
+  return (
+    numberOfAscents > 0 && (
+      <div className="table">
+        <h3>Ascents</h3>
+        <p>
+          {`${numberOfAscents} ascents over
+          ${numberOfAscentDays} days`}
+        </p>
+        <table className="ascent-table">
+          <thead>
+            <tr>
+              <th>Route Name</th>
+              <th>Topo Grade</th>
+              <th>Number Of Tries</th>
+              <th>Crag</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {numberOfAscents > 0 &&
+              ascentList.map(
+                ({
+                  routeName,
+                  topoGrade,
+                  date,
+                  crag,
+                  routeOrBoulder,
+                  numberOfTries,
+                  id,
+                }) => (
+                  <tr
+                    key={id}
+                    className={`${routeOrBoulder} ${topoGrade} ${numberOfTries}`}
+                  >
+                    <td>
+                      {`${
+                        routeOrBoulder === 'route' ? '🧗‍♂️' : '🪨'
+                      } ${routeName}`}
+                    </td>
+                    <td>{topoGrade}</td>
+                    <td>{numberOfTries}</td>
+                    <td>{crag}</td>
+                    <td>{dayjs(date).format('YYYY-MM-DD')}</td>
+                  </tr>
+                )
+              )}
+          </tbody>
+        </table>
+      </div>
+    )
+  )
 }
